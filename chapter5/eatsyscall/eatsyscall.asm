39546f2a57871d20a6ae4120f684af1360bce626
Jay Rajput <jayrajput@gmail.com>
2016-05-20 03:42:20 +0000

    removed tabs

--- chapter5/eatsyscall/eatsyscall.asm
+++ chapter5/eatsyscall/eatsyscall.asm
@@ -1,22 +1,22 @@
 ; See http://0xax.blogspot.com/2014/08/say-hello-to-x64-assembly-part-1.html
-SECTION .data			; Section containing initialised data
-	
-	EatMsg db "Eat at Joe's!"
-	
-SECTION .text			; Section containing code
+SECTION .data      ; Section containing initialised data
 
-global 	_start			; Linker needs this to find the entry point!
-	
+    EatMsg db "Eat at Joe's!"
+
+SECTION .text      ; Section containing code
+
+global     _start  ; Linker needs this to find the entry point!
+
 _start:
-	nop            ; This no-op keeps gdb happy...
-	mov rax,1      ; Specify sys_write call
-	mov rdi,1      ; Specify File Descriptor 1: Standard Output
-	mov rsi,EatMsg ; Pass offset of the message
-	mov rdx,13     ; Pass the length of the message
-	syscall
+    nop            ; This no-op keeps gdb happy...
+    mov rax,1      ; Specify sys_write call
+    mov rdi,1      ; Specify File Descriptor 1: Standard Output
+    mov rsi,EatMsg ; Pass offset of the message
+    mov rdx,13     ; Pass the length of the message
+    syscall
     ;int 80H       ; This does not work on x86_64
 
-	mov rax,60		; Code for Exit Syscall
-	mov rdi,60		; Return a code of zero	
+    mov rax,60        ; Code for Exit Syscall
+    mov rdi,60        ; Return a code of zero    
     syscall
-    ;int 80H	    ; This does not work on x86_64
+    ;int 80H        ; This does not work on x86_64

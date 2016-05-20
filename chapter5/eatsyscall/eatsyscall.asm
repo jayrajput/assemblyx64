09c000018ef1b5fc8556d0c9f608917bbd99c852
Jay Rajput <jayrajput@gmail.com>
2016-05-20 03:41:02 +0000

    updates

--- chapter5/eatsyscall/eatsyscall.asm
+++ chapter5/eatsyscall/eatsyscall.asm
@@ -1,62 +1,22 @@
-;  Executable name : EATSYSCALL
-;  Version         : 1.0
-;  Created date    : 1/7/2009
-;  Last update     : 2/18/2009
-;  Author          : Jeff Duntemann
-;  Description     : A simple program in assembly for Linux, using NASM 2.05,
-;    demonstrating the use of Linux INT 80H syscalls to display text.
-;
-;  Build using these commands:
-;    nasm -f elf -g -F stabs eatsyscall.asm
-;    ld -o eatsyscall eatsyscall.o
-;
-
+; See http://0xax.blogspot.com/2014/08/say-hello-to-x64-assembly-part-1.html
 SECTION .data			; Section containing initialised data
 	
-	EatMsg db "hello, world!"
+	EatMsg db "Eat at Joe's!"
 	
 SECTION .text			; Section containing code
 
 global 	_start			; Linker needs this to find the entry point!
 	
 _start:
-	nop			; This no-op keeps gdb happy...
-	mov rax,1		; Specify sys_write call
-	mov rdi,1		; Specify File Descriptor 1: Standard Output
-	mov rsi,EatMsg		; Pass offset of the message
-	mov rdx,13		; Pass the length of the message
-    syscall
-    ;int 80H
+	nop            ; This no-op keeps gdb happy...
+	mov rax,1      ; Specify sys_write call
+	mov rdi,1      ; Specify File Descriptor 1: Standard Output
+	mov rsi,EatMsg ; Pass offset of the message
+	mov rdx,13     ; Pass the length of the message
+	syscall
+    ;int 80H       ; This does not work on x86_64
 
 	mov rax,60		; Code for Exit Syscall
 	mov rdi,60		; Return a code of zero	
     syscall
-	;int 80H			; Make kernel call
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ;int 80H	    ; This does not work on x86_64
